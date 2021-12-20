88cc51c34fa723cb964879aa5a4f830f867ff476
장형렬 <redblood778@naver.com>
2021-12-20 13:05:34 +0900

    setting 변경

---
+++ config/settings/local.py
@@ -0,0 +1,3 @@
+from .base import * #base.py 파일을 그대로 사용
+
+ALLOWED_HOSTS = [] # ALLOWED_HOSTS만 다르게 사용, 개발 환경에서 사용할 주소
---
+++ config/settings/prod.py
@@ -0,0 +1,3 @@
+from .base import *
+
+ALLOWED_HOSTS = ['3.38.32.54'] # 서버 환경에 사용 할 주소
